c081b20997af03abaf99e88d8d8bb0669a244585
star01234 <111554691+star01234@users.noreply.github.com>
2023-02-16 16:43:32 +0700

    Add files via upload

--- SE-RESTO/index.js
+++ SE-RESTO/index.js
@@ -17,3 +17,19 @@ close_icon.onclick = () =>{
     let search_form = document.querySelector("#search-form");
     search_form.classList.remove('active')
 }
+
+//swiper function
+var swiper = new Swiper(".home-slider", {
+    spaceBetween:30,
+    centeredSlides:true,
+    autoplay:{
+        delay:1000,   //เวลาในการเลื่อนสไลด์
+        disableOnInteraction:false,
+    },
+    pagination :{
+        el:".swiper-pagination",
+        cilckable:true,
+    },
+    loop:true
+
+});
