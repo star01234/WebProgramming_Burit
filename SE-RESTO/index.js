bdb22eb8e91141cab0aebdd773a239848a1e3587
star01234 <burit.srisawan@gmail.com>
2023-02-23 16:45:01 +0700

    16.44

--- SE-RESTO/index.js
+++ SE-RESTO/index.js
@@ -33,3 +33,31 @@ var swiper = new Swiper(".home-slider", {
     loop:true
 
 });
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween:30,
+    centeredSlides:true,
+    autoplay:{
+        delay:1000,   //เวลาในการเลื่อนสไลด์
+        disableOnInteraction:false,
+    },
+    pagination :{
+        el:".swiper-pagination",
+        cilckable:true,
+    },
+    loop:true,
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        640:{
+            slidesPerView:2,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        }
+    }
+});
