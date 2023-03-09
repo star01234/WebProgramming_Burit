8bfb2a9206a94bf16d57476f1fd826751d4f374c
star01234 <burit.srisawan@gmail.com>
2023-03-09 10:31:37 +0700

    10.30

--- SE-RESTO/index.js
+++ SE-RESTO/index.js
@@ -61,3 +61,13 @@ var swiper = new Swiper(".review-slider", {
         }
     }
 });
+
+ //Loader Section
+function loader(){
+    document.querySelector(".loader-container").classList.add("fade-out")
+}
+function fadeOut(){
+    setInterval(loader, 3000);
+}
+ 
+ window.onload = fadeOut;
