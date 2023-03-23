015024e1bc23fb6ca30c7b3b2c481a49a1b9c259
star01234 <burit.srisawan@gamil.com>
2023-03-23 17:35:00 +0700

    17

--- Adex/assets/js/script.js
+++ Adex/assets/js/script.js
@@ -1,71 +1,91 @@
-const addEventOnElements = (elements, evenType, callback) => {
-  for (let i = 0, len = elements.length; i < len; i++) {
-    elements[i].addEventListener(evenType, callback);
-  }
-};
+const addEventOnElements = (elements, evenType, callback)=>{
+    for (let i = 0, len = elements.length; i<len; i++) {
+        elements[i].addEventListener(evenType, callback);
+    }
+}
 
-/**
- * NavBar toggle
- */
-const navbar = document.querySelector("[data-navbar]");
-const navTogglers = document.querySelectorAll("[data-nav-toggler]");
-const overlay = document.querySelector("[data-overlay]");
 
-const toggleNavbar = () => {
-  navbar.classList.toggle("active");
-  overlay.classList.toggle("active");
-  document.body.classList.toggle("nav-active");
-};
+/** 
+ * Navbar toggle
+*/
+
+const navbar = document.querySelector("[data-navbar]")
+const navTogglers = document.querySelectorAll("[data-nav-toggler]")
+const overlay = document.querySelector("[data-overlay]")
+
+const toggleNavbar = ()=>{
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
 addEventOnElements(navTogglers, "click", toggleNavbar);
 
 /**
  * Slider
  */
-const sliders = document.querySelectorAll("[data-slider]");
+const sliders = document.querySelectorAll("[data-slider]")
+const initSlider = (currentSlider) =>{
+    const sliderContainer = currentSlider.querySelector("[data-slider-container]");
+    const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
+     const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
+     let currentSliderPo = 0;
 
-const initSlider = (currentSlider) => {
-  const sliderContainer = currentSlider.querySelector(
-    "[data-slider-container]"
-  );
-  const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
-  const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
-  let currentSliderPo = 0;
+     const moveSliderItem = () =>{
+        sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSliderPo].offsetLeft}px)`;
+     };
+// Previous
+     const slidePrev = () =>{
+        if(currentSliderPo <= 0){
+            currentSliderPo = sliderContainer.childElementCount - 1;
+        }else{
+            currentSliderPo--;
+        }
+        moveSliderItem();
+     };
+     sliderPrevBtn.addEventListener("click", slidePrev);
 
-  const moveSliderItem = () => {
-    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSliderPo].offsetLeft}px)`;
-  };
-  // Previous
-  const slidePrev = () => {
-    if (currentSliderPo <= 0) {
-      currentSliderPo = sliderContainer.childElementCount - 1;
-    } else {
-      currentSliderPo--;
-    }
-    moveSliderItem();
-  };
-  sliderPrevBtn.addEventListener("click", slidePrev);
+     //Next
+    const slideNext = () =>{
+        const slideEnd = currentSliderPo >= sliderContainer.childElementCount - 1;
 
-  //Next
-  const slideNext = () => {
-    const slideEnd = currentSliderPo >= sliderContainer.childElementCount - 1;
+        if(slideEnd){
+            currentSliderPo = 0;
+        }else{
+            currentSliderPo++;
+        }
+        moveSliderItem();
+     };
+     sliderNextBtn.addEventListener("click", slideNext);
 
-    if (slideEnd) {
-      currentSliderPo = 0;
-    } else {
-      currentSliderPo++;
-    }
-    moveSliderItem();
-  };
-  sliderNextBtn.addEventListener("click", slideNext);
-
-  //No more Slide
-  const dontHaveExtraItem = sliderContainer.childElementCount <= 1;
-  if (dontHaveExtraItem) {
-    sliderNextBtn.style.display = "none";
-    sliderPrevBtn.style.display = "none";
-  }
+     //No more Silde
+     const dontHaveExtraItem = sliderContainer.childElementCount <=1;
+     if(dontHaveExtraItem){
+        sliderNextBtn.style.display = "none";
+        sliderPrevBtn.style.display = "none";
+     }
 };
 
 for(let i=0, len = sliders.length; i<len; i++){
     initSlider(sliders[i]);
+}
+
+//Accordian
+
+const accordians = document.querySelectorAll("[data-accordian]");
+let lastActiveAccordian = accordians[0];
+const initAccordian = (currentAccordian) =>{
+  const accordianBtn = currentAccordian.querySelector("[data-accordian-btn]");
+  const expandAccordian = () => {
+    if (lastActiveAccordian && lastActiveAccordian != currentAccordian) {
+      currentAccordian.classList.remove("expanded");
+    }
+    currentAccordian.classList.toggle("expanded");
+    lastActiveAccordian = currentAccordian;
+  };
+  accordianBtn.addEventListener("click",expandAccordian);
+};
+
+for(let i = 0, len=accordians.length; i<len;i++){
+  initAccordian(accordians[i]);
 }
